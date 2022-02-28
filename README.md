ea4d4eeb25eee2ee90c7a3ae527e53be2cfc0824
David <d@nogare.cz>
2022-03-01 00:14:30 +0100

    research on functionality

---
+++ README.md
@@ -0,0 +1,10 @@
+Plugin for MS Word.
+
+Dev install and run:
+--------------------
+Powershell (as admin)
+```
+git clone ${this repo}
+cd '.\Lindat-Translation-Plugin\'
+npm start
+```
---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,61 @@
+document.getElementById("translateCsEn").addEventListener("click", translateSelection)
+
+function translateSelection() {
+	Word.run(function (context) {
+			const doc = context.document
+			let originalRange = doc.getSelection()
+			originalRange.load("text")
+
+			return context.sync()
+				.then(function () {
+					sendToLindatAPI("cs", "en", originalRange.text)
+						.then(response => response.text())
+						.then(data => {
+							data = data.replace(/\s+$/g, '') // replace newline on end
+							doc.body.insertParagraph(data, "End")
+						})
+						.then(context.sync)
+						.catch((error) => {
+							console.error('Error:', error)
+						})
+
+				})
+		})
+		.catch(function (error) {
+			console.log("Error: " + error)
+			if (error instanceof OfficeExtension.Error) {
+				console.log("Debug info: " + JSON.stringify(error.debugInfo))
+			}
+		})
+}
+
+function sendToLindatAPI(src, tgt, input_text) {
+	const url = "https://lindat.mff.cuni.cz/services/translation/api/v2/languages/";
+
+	const formData = {
+		src,
+		tgt,
+		input_text,
+	}
+
+
+	let formBody = [];
+	for (let property in formData) {
+		const encodedKey = encodeURIComponent(property)
+		const encodedValue = encodeURIComponent(formData[property])
+		formBody.push(encodedKey + "=" + encodedValue)
+	}
+	formBody = formBody.join("&");
+	
+	const headers = {
+		'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+	}
+
+	const response = fetch(url, {
+		method: 'POST',
+		headers,
+		body: formBody
+	})
+
+	return response;
+}
